a8dc75393b54cae3dc52d78762b9b1ec5a4451c1
Anton Nocum <anton.nocum@gmail.com>
2020-12-05 03:44:38 +0800

    Tutorial 1

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import React from "react"
+import Header from "../components/header"
+
+export default function About() {
+  return (
+    <div style={{ color: `teal` }}>
+      <Header headerText="About Gatsby" />
+      <Header headerText="Cool" />
+      <p>Such wow. Ver'y React.</p>
+    </div>
+  )
+}
